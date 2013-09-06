a25f0118ee5141ceb0c0e20c694c47a480a0023f
Rudy Grigar <rudy@grigar.net>
2013-09-06 13:33:41 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# osl-nginx cookbook
+
+# Requirements
+* nginx
+
+# Usage
+* `nginx::repo` to add the nginx repository for centos
+
+# Attributes
+
+# Recipes
+* `nginx::repo`
+
+# Author
+
+Author:: OSU Open Source Lab (<systems@osuosl.org>)
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'osl-nginx'
+maintainer       'OSU Open Source Lab'
+maintainer_email 'systems@osuosl.org'
+license          'Apache 2.0'
+description      'Installs/Configures osl-nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'nginx'
