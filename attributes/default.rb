a81ff47e6cf5aa4e20949c30034ad6e94dec240a
armiller <armiller@osuosl.org>
2014-03-05 15:15:10 -0800

    Add enabled and disabled sites

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,8 @@
 default['osl-nginx']['recipes'] = ["default"]
+default['osl-nginx']['hostname'] = ["default"]
+default['osl-nginx']['hostsites']['enabled'] = nil
+default['osl-nginx']['hostsites']['disabled'] = nil
+
+
 
 default['nginx']['repo_source'] = "nginx"
