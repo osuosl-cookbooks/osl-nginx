9f99613e252a675a7d6d92c5baeeb3acc1028e74
armiller <armiller@osuosl.org>
2014-03-07 14:21:54 -0800

    Add if case when there is not sites

--- recipes/vhosts.rb
+++ recipes/vhosts.rb
@@ -18,30 +18,32 @@
 #
 include_recipe "osl-nginx::default"
 
-#Set up log directories
-node['osl-nginx']['hostsites']['enabled'].each do |ensite|
-    directory "#{node['nginx']['log_dir']}/#{ensite}/access" do 
-        owner "root"
-        group "root"
-        mode 00644
-        recursive true
-        action :create
-    end
-    directory "#{node['nginx']['log_dir']}/#{ensite}/error" do 
-        owner "root"
-        group "root"
-        mode 00644
-        action :create
-    end
-    cookbook_file "#{node['nginx']['dir']}/sites-available/#{ensite}.conf" do
-        source "#{node['osl-nginx']['hostname']}/#{ensite}.conf"
-        mode "0644"
-        owner "root"
-        group "root"
-        notifies :reload, "service[nginx]"
-    end
-    nginx_site "#{ensite}.conf" do
-        action :enable
+# Loop over enabled sites
+if node['osl-nginx']['hostsites']['enabled'] then
+    node['osl-nginx']['hostsites']['enabled'].each do |ensite|
+        directory "#{node['nginx']['log_dir']}/#{ensite}/access" do 
+            owner "root"
+            group "root"
+            mode 00644
+            recursive true
+            action :create
+        end
+        directory "#{node['nginx']['log_dir']}/#{ensite}/error" do 
+            owner "root"
+            group "root"
+            mode 00644
+            action :create
+        end
+        cookbook_file "#{node['nginx']['dir']}/sites-available/#{ensite}.conf" do
+            source "#{node['osl-nginx']['hostname']}/#{ensite}.conf"
+            mode "0644"
+            owner "root"
+            group "root"
+            notifies :reload, "service[nginx]"
+        end
+        nginx_site "#{ensite}.conf" do
+            action :enable
+        end
     end
 end
 
