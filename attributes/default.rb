540fcef62eb29d816a7eefd283b5f505c4206a2d
armiller <armiller@osuosl.org>
2014-03-10 14:37:19 -0700

    Update defaults

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,7 @@ default['osl-nginx']['hostname'] = nil || node['fqdn']
 default['osl-nginx']['hostsites']['enabled'] = nil
 default['osl-nginx']['hostsites']['disabled'] = nil
 
-
+default['osl-nginx']['hostsites']['log_dir'] = node['nginx']['log_dir']
+default['osl-nginx']['hostsites']['default_root'] = node['nginx']['default_root']
 
 default['nginx']['repo_source'] = "nginx"
