6077b5299abbad6a1c0626ba749162803691d1ee
armiller <armiller@osuosl.org>
2014-03-13 15:47:43 -0700

    Fix defaults

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
 default['osl-nginx']['recipes'] = ["default"]
-default['osl-nginx']['hostname'] = nil || node['fqdn']
-default['osl-nginx']['hostsites']['enabled'] = nil
-default['osl-nginx']['hostsites']['disabled'] = nil
+default['osl-nginx']['hostname'] = node['fqdn']
+default['osl-nginx']['hostsites']['enabled'] = []
+default['osl-nginx']['hostsites']['disabled'] = []
 
 default['osl-nginx']['hostsites']['log_dir'] = node['nginx']['log_dir']
-default['osl-nginx']['hostsites']['default_root'] = node['nginx']['default_root']
+default['osl-nginx']['hostsites']['default_root'] = "/var/www"
 
 default['nginx']['repo_source'] = "nginx"
