20c1a9dd7f1dfe9b34c25272f00a8167e3194b31
armiller <armiller@osuosl.org>
2014-03-13 15:48:00 -0700

    Create dirs, template and include

--- recipes/vhosts.rb
+++ recipes/vhosts.rb
@@ -18,15 +18,59 @@
 #
 include_recipe "osl-nginx::default"
 
-if default['osl-nginx']['hostsites']['enabled'] then
-    default['osl-nginx']['hostsites']['enabled'].each do |site|
-        template "#{site['server_name']}" do 
-
-
-
-
-
-
-        end 
+node['osl-nginx']['hostsites']['enabled'].each do |key, site|
+   log_dir = site.fetch("directory", "#{node['osl-nginx']['hostsites']['default_root']}/#{site.fetch('name')}") 
+    directory log_dir do 
+        owner "root"
+        group "root"
+        mode 00644
+        recursive true
+        action :create
+    end
+    directory "#{node['osl-nginx']['hostsites']['log_dir']}/#{key}/access" do 
+        owner "root"
+        group "root"
+        mode 00644
+        recursive true
+        action :create
+    end
+    directory "#{node['osl-nginx']['hostsites']['log_dir']}/#{key}/error" do 
+        owner "root"
+        group "root"
+        mode 00644
+        action :create
+    end
+    if site.has_key?("custom_config") then
+        vhost_include = "#{node['nginx']['dir']}/sites-available/#{key}_include.conf"
+        cookbook_file vhost_include do
+            source "#{node['osl-nginx']['hostname']}/#{key}.conf" 
+            owner "root"
+            group "root"
+            mode 0644
+            notifies :reload, "service[nginx]"
+        end
+    end
+    template "#{node['nginx']['dir']}/sites-available/#{key}.conf" do
+        source "nginx_app.conf.erb"
+        owner "root"
+        group "root"
+        mode 0644
+        notifies :reload, "service[nginx]"
+        variables({ 
+            :vhost => site,
+            :server_name => key,
+            :server_aliases => site.fetch("aliases"),
+            :directory => log_dir,
+            :custom_config => vhost_include
+        })
+    end 
+    nginx_site "#{site.fetch("name")}.conf" do
+        action :enable
     end
 end 
+
+node['osl-nginx']['hostsites']['disabled'].each do
+    nginx_site "#{site['name']}" do
+        action :disable
+    end
+end
