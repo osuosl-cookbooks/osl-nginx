070b8a73c645b27dd38e894002b773825f01efb9
armiller <armiller@osuosl.org>
2014-03-28 11:40:35 -0700

    Initial lwrp

---
+++ definitions/nginx_app.rb
@@ -0,0 +1,82 @@
+#
+# Cookbook Name:: osl-nginx
+# lwrp:: nginx_app
+#
+# Copyright (C) 2013 Oregon State University
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+define :nginx_app, :template => "nginx_app.conf.erb", :local => false, :enable => true do
+
+    application_name = params[:name]
+    application_directory = params[:web_root]
+
+    include_recipe "osl-nginx::default"
+    
+    directory application_directory do 
+        owner if params[:owner] ? params[:owner] : "root"
+        group "root"
+        mode 00644
+        recursive true
+        action :create
+    end
+    directory "#{node['nginx']['log_dir']}/#{key}/access" do 
+        owner "root"
+        group "root"
+        mode 00644
+        recursive true
+        action :create
+    end
+    directory "#{node['nginx']['log_dir']}/#{key}/error" do 
+        owner "root"
+        group "root"
+        mode 00644
+        action :create
+    end
+    if site.has_key?("custom_config") then
+        vhost_include = "#{node['nginx']['dir']}/sites-available/#{key}_include.conf"
+        cookbook_file vhost_include do
+            source "#{node['osl-nginx']['hostname']}/#{key}.conf" 
+            owner "root"
+            group "root"
+            mode 0644
+            if ::File.exists?("#{node['nginx']['dir']}/sites-enabled/#{key}.conf")
+                notifies :reload, "service[nginx]"
+            end
+        end
+    end
+    template "#{node['nginx']['dir']}/sites-available/#{key}.conf" do
+        source "nginx_app.conf.erb"
+        owner "root"
+        group "root"
+        mode 0644
+        variables({ 
+            :server_name => key,
+            :server_aliases => site.fetch("aliases", []),
+            :directory => web_root,
+            :custom_config => vhost_include,
+            :custom_logs => site.has_key?("custom_logs")
+        })
+        if ::File.exists?("#{node['nginx']['dir']}/sites-enabled/#{key}.conf")
+            notifies :reload, "service[nginx]"
+        end
+    end 
+    nginx_site "#{key}.conf" do
+        if site.has_key?("disable") then
+            enable false
+        else 
+            enable true
+        end
+    end
+end 
