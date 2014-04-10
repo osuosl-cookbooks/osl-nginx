ea68cd4e82961196550c042977fb0b508841f3f9
armiller <armiller@osuosl.org>
2014-04-10 15:39:48 -0700

    Fix path

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'systems@osuosl.org'
 license          'Apache 2.0'
 description      'Installs/Configures osl-nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.3'
+version          '1.0.4'
 depends          'nginx', "< 2.1.0"
 depends          'firewall'
 depends          'logrotate'
