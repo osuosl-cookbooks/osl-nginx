b9a8e5a5a37b2ecb11a13a4d83d1e7c6a2cc140c
Manatee <github@osuosl.org>
2017-08-23 20:29:00 +0000

    Automatic minor-level version bump to v2.1.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ osl-nginx CHANGELOG
 This file is used to list changes made in each version of the
 osl-nginx cookbook.
 
+2.1.0 (2017-08-23)
+------------------
+- Massive cleanup
+
 2.0.0 (2017-07-03)
 ------------------
 - Switch to chef_nginx
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ source_url       'https://github.com/osuosl-cookbooks/osl-nginx'
 license          'Apache 2.0'
 description      'Installs/Configures osl-nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.1.0'
 
 depends          'certificate'
 depends          'firewall'
