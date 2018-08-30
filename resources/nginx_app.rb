5dd0c0e11eedb7f114a931447f48b11acae124ac
Jason Chen <jasonc@osuosl.org>
2018-08-30 14:58:10 -0700

    Fix nginx_app custom resource

--- resources/nginx_app.rb
+++ resources/nginx_app.rb
@@ -8,13 +8,13 @@ property :cookbook, String, default: 'osl-nginx'
 property :cookbook_include, [String, nil], default: nil
 property :custom_logs, [true, false], default: false
 property :server_port, [String, Integer], default: 80
-property :directory, [String, nil], coerce: proc { |d| d.nil? ? "/var/www/#{name}" : d }
+property :directory, String, default: lazy { "/var/www/#{name}" }
 property :directive_http, [Array, nil], default: nil
 property :directive_https, [Array, nil], default: nil
 property :directory_index, [String, nil], default: nil
 property :enable, [true, false], default: true
 property :include_config, [true, false], default: false
-property :include_name, [String, nil], default: nil
+property :include_name, String, default: lazy { name }
 property :include_resource, String, default: 'cookbook_file'
 property :local, [true, false], default: false
 property :server_aliases, Array, default: []
@@ -24,20 +24,18 @@ property :template, String, default: 'nginx_app.conf.erb'
 action :create do
   include_recipe 'osl-nginx::default'
 
-  directory "#{node['nginx']['log_dir']}/#{name}/access" do
+  declare_resource(:directory,"#{node['nginx']['log_dir']}/#{name}/access") do
     owner 'root'
     group 'root'
     mode 0644
     recursive true
   end
 
-  directory "#{node['nginx']['log_dir']}/#{name}/error" do
+  declare_resource(:directory, "#{node['nginx']['log_dir']}/#{name}/error") do
     owner 'root'
     group 'root'
     mode 0644
   end
-
-  include_name = include_name.nil? ? name : include_name
 
   if include_config
     vhost_include = ::File.join(node['nginx']['dir'], 'sites-available', "#{include_name}_include.conf")
@@ -75,27 +73,32 @@ action :create do
 
   declare_resource(:template, "#{node['nginx']['dir']}/sites-available/#{name}.conf") do
     source template
-    cookbook cookbook
+    cookbook new_resource.cookbook
     owner node['nginx']['user']
     group node['nginx']['group']
     mode 0644
     variables(
       server_aliases: server_aliases,
-      params: self.class.properties(false)
+      params: all_params
     )
 
-    if ::File.exist?(::File.join(node['nginx']['dir'], 'sites-enabled', "#{include_name}.conf"))
+    if ::File.exist?(::File.join(node['nginx']['dir'], 'sites-enabled', "#{name}.conf"))
       notifies :reload, 'service[nginx]'
     end
   end
 
   nginx_site "#{name}.conf" do
-    action :enable if enable
+    if new_resource.enable
+      action :enable
+    else
+      action :disable
+    end
   end
 end
 
-action :disable do
-  nginx_site "#{name}.conf" do
-    action :disable
+def all_params
+  this = self
+  self.class.properties(true).keys.each_with_object({}) do |prop, acc|
+    acc[prop] = this.send(prop)
   end
 end
