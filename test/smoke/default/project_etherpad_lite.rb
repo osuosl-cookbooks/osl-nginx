83b6680b473e20c9eb6d08f2d3008d47c27ff4bf
Andrew Dassonville <andrewda@osuosl.org>
2021-02-12 12:35:33 -0800

    Deprovision etherpad-lite

--- test/smoke/default/project_etherpad_lite.rb
+++
@@ -1,4 +0,0 @@
-# Inspec test for recipe osl-nginx::project_etherpad_lite
-
-# The Inspec reference, with examples and extensive documentation, can be
-# found at http://inspec.io/docs/reference/resources/
