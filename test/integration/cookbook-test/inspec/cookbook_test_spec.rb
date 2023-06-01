f5a34cc66a2b0a59ad9b2a35e3053dd669e983f9
Zachary Wallace <zwall@osuosl.org>
2023-06-01 11:47:21 -0700

    Cookstyle patch

--- test/integration/cookbook-test/inspec/cookbook_test_spec.rb
+++ test/integration/cookbook-test/inspec/cookbook_test_spec.rb
@@ -103,5 +103,5 @@ describe file '/etc/nginx/conf.http.d/list.conf' do
 end
 
 describe file '/etc/nginx/conf.d/test-kitchen.conf' do
-  its('content') { should match "server_names_hash_bucket_size 128" }
+  its('content') { should match 'server_names_hash_bucket_size 128' }
 end
